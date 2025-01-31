62eb0d1eb40611721a9457d0c1a951688a2eadee
abdlaziz15bnaame <abdlaziz15bnaame@gmail.com>
2025-01-31 19:27:53 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,3 +9,9 @@ window.onscroll = function(){
         header.classList.remove("active")
     }
 }
+
+let nav_links = document.getElementById("links");
+
+function Open_Close_Menu() {
+    nav_links.classList.toggle("active")
+}
